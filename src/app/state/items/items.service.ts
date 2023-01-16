4e5740bb8196f8fa9937166580382520ca2e544c
Igor Nerozya <nerozya.igor@gmail.com>
2023-01-16 14:15:07 +0200

    Fixed delete item

--- src/app/state/items/items.service.ts
+++ src/app/state/items/items.service.ts
@@ -10,7 +10,7 @@ export class ItemsService {
     private itemsRequester = this.requesterFactory.createRequester<void, IItem[]>({ url: `${Config.apiUrl}/items`, method: HttpMethod.GET });
     private createNewItemRequester = this.requesterFactory.createRequester<IItem, void>({ url: `${Config.apiUrl}/items`, method: HttpMethod.POST });
     private updateItemRequester = this.requesterFactory.createRequester<IItem, void>({ url: `${Config.apiUrl}/items/{id}`, method: HttpMethod.PATCH });
-    private deleteItemRequester = this.requesterFactory.createRequester<{ id: string }, void>({ url: `${Config.apiUrl}/item/{id}`, method: HttpMethod.DELETE });
+    private deleteItemRequester = this.requesterFactory.createRequester<{ id: string }, void>({ url: `${Config.apiUrl}/items/{id}`, method: HttpMethod.DELETE });
 
     constructor(private requesterFactory: RequesterFactory) {
     }
